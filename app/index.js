e44044277a37a916f6c0e88f76abab3112614f62
ogcule <ogcule@outlook.com>
2017-08-22 16:56:02 +0100

    update files

--- app/index.js
+++ app/index.js
@@ -4,5 +4,16 @@ require('./index.css');
 
 
 class App extends React.Component {
-
+  render(){
+    return{
+      <div>
+        Hello World!
+      </div>
+    };
+   }
 }
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
