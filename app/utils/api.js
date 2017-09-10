603628c69e30d02f5bdad721bb393ef9f4451496
ogcule <ogcule@outlook.com>
2017-09-10 20:01:46 +0100

    update

---
+++ app/utils/api.js
@@ -0,0 +1,12 @@
+var axios = require('axis');
+
+module.exports = {
+  fetchPopularRepos: function (language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+    return axios.get(encodedURI)
+      .then(function (response) {
+        return response.data.items;
+      });
+  }
+};
